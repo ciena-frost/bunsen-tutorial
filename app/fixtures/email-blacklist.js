6ec03f7a8aa302d120d0ceef27f58740f13a64ff
Matthew Dahl <mdahl@ciena.com>
2016-06-03 20:43:21 -0700

    Add custom validator

---
+++ app/fixtures/email-blacklist.js
@@ -0,0 +1,8 @@
+export default [
+  // Blacklist internet trolls
+  'internettroll@example.com',
+
+  // Blacklist certain government organizations (tin-foil hats welcome)
+  'fbi.gov',
+  'cia.gov'
+];
